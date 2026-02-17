3e2af37c5643f5eee8f3737488f182043a3d2aac
Aaron O'Brien <aobpc2000@gmail.com>
2026-02-17 13:06:10 -0500

    Implemented gunicorn for production deployment

--- src/localfinds/app.py
+++ src/localfinds/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 from database.posts_db import initialize_db, store_post, get_post, get_all_posts, clear_posts, posts_db
 
@@ -55,4 +56,5 @@ def create_post():
 # ------------Run App------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
